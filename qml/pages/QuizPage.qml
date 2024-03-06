tomin-apps/countryquiz

commit b309f92b1d3b9bf4fe5d9fb9198bcb9d6fb947b7

qml: Improve QuizPage header
Instead of 'Quiz' say the type of the quiz there. Use the header space
on devices that otherwise wouldn't have enough space on the top to show
the map on full width.

Signed-off-by: Tomi Leppänen <tomi@tomin.site>

qml/pages/QuizPage.qml
@@ -33,19 +33,43 @@ Page {
 
     onStatusChanged: if (status === PageStatus.Active) choices.opacity = 1.0
 
+    PageHeader {
+        readonly property string name: {
+            if (setup.quizType === "flags") {
+                return qsTr("Flag")
+            } if (setup.quizType === "maps") {
+                return qsTr("Map")
+            } if (setup.quizType === "capitals") {
+                return qsTr("Capital City")
+            }
+            return ""
+        }
+
+        id: header
+        title: qsTr("%1 %2 / %3").arg(name).arg(page.current).arg(page.count)
+        z: 2
+    }
+
     Column {
         readonly property int otherHeight: header.height + label.height + timeLeft.height
         readonly property bool otherReady: header.height !== 0 && label.height !== 0 && timeLeft.height !== 0
+        property bool useHeaderSpace
         id: column
+        anchors.top: useHeaderSpace ? page.top : header.bottom
         width: parent.width
-
-        PageHeader {
-            id: header
-            title: qsTr("Quiz (%1 / %2)").arg(page.current).arg(page.count)
-        }
+        z: 1
 
         Loader {
-            property int maximumHeight: Math.min(parent.width, page.height - column.otherHeight - choices.minimumHeight)
+            property int maximumHeight: {
+                var maximum = Math.min(parent.width, page.height - column.otherHeight - choices.minimumHeight)
+                if (setup.quizType === "maps" && maximum < parent.width) {
+                    column.useHeaderSpace = true
+                    return maximum + header.height
+                } else {
+                    column.useHeaderSpace = false
+                }
+                return maximum
+            }
             property int maximumWidth: parent.width
             property bool ready: parent.width !== 0 && page.height !== 0 && column.otherReady && choices.minimumHeight !== 0
 

translations/harbour-countryquiz-fi.ts
@@ -71,42 +71,57 @@
 <context>
     <name>QuizPage</name>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="44"/>
-        <source>Quiz (%1 / %2)</source>
+        <location filename="../qml/pages/QuizPage.qml" line="39"/>
+        <source>Flag</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="63"/>
+        <location filename="../qml/pages/QuizPage.qml" line="41"/>
+        <source>Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/QuizPage.qml" line="43"/>
+        <source>Capital City</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/QuizPage.qml" line="49"/>
+        <source>%1 %2 / %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/QuizPage.qml" line="87"/>
         <source>Guess which country this flag belongs to</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="66"/>
+        <location filename="../qml/pages/QuizPage.qml" line="90"/>
         <source>Guess which country is highlighted on the map</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="70"/>
+        <location filename="../qml/pages/QuizPage.qml" line="94"/>
         <source>Guess which country&apos;s capital is this</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="72"/>
+        <location filename="../qml/pages/QuizPage.qml" line="96"/>
         <source>Guess which country&apos;s capitals are these</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="291"/>
+        <location filename="../qml/pages/QuizPage.qml" line="315"/>
         <source>%1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="293"/>
+        <location filename="../qml/pages/QuizPage.qml" line="317"/>
         <source>%1 and %2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/QuizPage.qml" line="295"/>
+        <location filename="../qml/pages/QuizPage.qml" line="319"/>
         <source>%1, %2 and %3</source>
         <translation type="unfinished"></translation>
     </message>

translations/harbour-countryquiz.ts
@@ -60,10 +60,6 @@
 <context>
     <name>QuizPage</name>
     <message>
-        <source>Quiz (%1 / %2)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Guess which country this flag belongs to</source>
         <translation type="unfinished"></translation>
     </message>
@@ -89,6 +85,22 @@
     </message>
     <message>
         <source>Guess which country&apos;s capitals are these</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Flag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 %2 / %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Capital City</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

commit 42de8e5d79af9b7fd9332ac30ec005070c3dc5c0

qml: Fix a regression from b309f92b
Flag quiz had issues on shorter devices due to forgetting to account for
dialog height as it was moved out of the column.

Signed-off-by: Tomi Leppänen <tomi@tomin.site>

qml/pages/QuizPage.qml
@@ -191,7 +191,7 @@ Page {
         id: choices
         anchors.bottom: parent.bottom
         boundsBehavior: Flickable.StopAtBounds
-        height: Math.min(maximumHeight, Math.max(minimumHeight, page.height - column.height))
+        height: Math.min(maximumHeight, Math.max(minimumHeight, page.height - column.height - (column.useHeaderSpace ? 0 : header.height)))
         model: DelegateModel {
             signal highlightCorrect
             signal highlightAllWrong