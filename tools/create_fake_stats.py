933d013ce0d7702709be209f9c4feea3fb697797
Tomi Leppänen <tomi@tomin.site>
2024-08-11 23:27:23 +0300

    tools: Improve create_fake_stats.py
    Add options for setting question count, language code and name. Also
    update presets to match the app.
    
    Signed-off-by: Tomi Leppänen <tomi@tomin.site>

--- tools/create_fake_stats.py
+++ tools/create_fake_stats.py
@@ -10,42 +10,49 @@ import datetime
 import itertools
 import pathlib
 import random
+import re
 import sqlite3
 from dataclasses import dataclass
 
-LENGTH = 15 # TODO: Allow choices
 MAX_SCORE = 1000
+MIN_SCORE = 200
+TIME_SCORE = 800
 
 @dataclass
 class Preset:
     type: str
-    count: int
     choices: int
     region: bool
     time: int
-    language: str
 
-    def into_data(self):
-        return [self.type, self.count, self.choices, 'same region' if self.region else 'everywhere', self.time, self.language]
+    def with_data(self, count, language):
+        return PresetWithData(self.type, self.choices, self.region, self.time, count, language)
 
     def preset(s):
-        # TODO: Support selecting length and language
         if s in PRESETS:
             return PRESETS[s]
         else:
             raise ValueError(f"'{s}' is not a preset")
 
 PRESETS = {
-    "flags-easy" : Preset("flags", 15, 3, False, 30, 'en'),
-    "flags-regular" : Preset("flags", 15, 4, False, 15, 'en'),
-    "flags-veteran" : Preset("flags", 15, 5, True, 15, 'en'),
-    "maps-easy" : Preset("maps", 15, 3, False, 30, 'en'),
-    "maps-regular" : Preset("maps", 15, 4, True, 30, 'en'),
-    "maps-veteran" : Preset("maps", 15, 5, True, 15, 'en'),
-    "capitals-easy" : Preset("capitals", 15, 3, False, 30, 'en'),
-    "capitals-regular" : Preset("capitals", 15, 4, False, 15, 'en'),
-    "capitals-veteran" : Preset("capitals", 15, 5, True, 15, 'en'),
+    "flags-easy" : Preset("flags", 3, False, 60),
+    "flags-regular" : Preset("flags", 4, False, 30),
+    "flags-veteran" : Preset("flags", 5, True, 15),
+    "maps-easy" : Preset("maps", 3, False, 60),
+    "maps-regular" : Preset("maps", 4, True, 30),
+    "maps-veteran" : Preset("maps", 5, True, 15),
+    "capitals-easy" : Preset("capitals", 3, False, 60),
+    "capitals-regular" : Preset("capitals", 4, False, 30),
+    "capitals-veteran" : Preset("capitals", 5, True, 15),
 }
+
+@dataclass
+class PresetWithData(Preset):
+    count: str
+    language: int
+
+    def into_data(self):
+        return [self.type, self.count, self.choices, 'same region' if self.region else 'everywhere', self.time, self.language]
 
 def division(total, pools):
     result = [int(total/pools)] * pools
@@ -78,56 +85,68 @@ def setup_database(file):
                    ")")
     return connection, cursor
 
-def insert_options(cursor, presets):
+def insert_options(cursor, presets, count, language):
     cursor.executemany("INSERT INTO options ( type, questions, choices, choices_from, time_to_answer, language ) "
-                       "VALUES ( ?, ?, ?, ?, ?, ? )", map(lambda x: x.into_data(), presets))
+                       "VALUES ( ?, ?, ?, ?, ?, ? )", map(lambda x: x.with_data(count, language).into_data(), presets))
 
-def generate_record(preset):
-    number_of_correct = random.randint(0, LENGTH)
+def generate_record(preset, name):
+    number_of_correct = random.randint(0, preset.count)
     total_time = 0
     score = 0
-    for i in range(LENGTH):
+    for i in range(preset.count):
         time = random.randint(0, preset.time * 1000)
         if i < number_of_correct:
-            score += MAX_SCORE * (1.0 - time / (preset.time * 1000))
+            score += MIN_SCORE + MAX_SCORE * (1.0 - time / (preset.time * TIME_SCORE))
         total_time += time
     end = datetime.datetime.now()
     start = end - datetime.timedelta(days=6*30.44)
     period = end - start
-    dt = start + datetime.timedelta(seconds=random.randint(0, period.total_seconds()))
-    return [number_of_correct, total_time, int(score), int(dt.timestamp())]
+    dt = start + datetime.timedelta(seconds=random.randint(0, int(period.total_seconds())))
+    return [number_of_correct, total_time, int(score), int(dt.timestamp()), name]
 
-def generate_records(preset, rows):
-    records = []
+def generate_records(preset, rows, name):
     for i in range(rows):
-        records.append(generate_record(preset))
-    return records
+        yield generate_record(preset, name)
 
-def insert_records(cursor, preset, rows):
-    records = generate_records(preset, rows)
+def insert_records(cursor, preset, rows, name):
+    records = generate_records(preset, rows, name)
     cursor.executemany(
         "INSERT INTO records (options, number_of_correct, time, score, datetime, name) "
-        "SELECT options.id, ?, ?, ?, ?, '' FROM options "
+        "SELECT options.id, ?, ?, ?, ?, ? FROM options "
         "WHERE type = ? AND questions = ? AND choices = ? AND choices_from = ? AND time_to_answer = ? AND language = ?",
         itertools.starmap(lambda a, b: a + b.into_data(), zip(records, itertools.repeat(preset))))
 
-def generate_database(file, rows, presets):
+def generate_database(file, rows, presets, count, language, name):
     rows_per_preset = division(rows, len(presets))
     connection, cursor = setup_database(file)
-    insert_options(cursor, presets)
+    insert_options(cursor, presets, count, language)
     for preset, rows in zip(presets, rows_per_preset):
-        insert_records(cursor, preset, rows)
+        insert_records(cursor, preset.with_data(count, language), rows, name)
     connection.commit()
+
+def language(s):
+    if re.match(r"^[a-z]{2}-[A-Z]{2}$", s) is None:
+        raise ValueError("'{s}' is not a valid language")
+    return s
+
+def count(s):
+    value = int(s)
+    if value <= 0:
+        raise ValueError("'{s}' is not a positive number")
+    return value
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('database_file', type=pathlib.Path)
-    parser.add_argument('--rows', '-r', type=int, default=1000)
+    parser.add_argument('--rows', '-r', type=count, default=1000)
     parser.add_argument('--preset', '-p', type=Preset.preset, action='append', dest='presets')
+    parser.add_argument('--count', '-c', type=count, default=15)
+    parser.add_argument('--language', '-l', type=language, default='en-GB')
+    parser.add_argument('--name', '-n', type=str, default='')
     args = parser.parse_args()
     if not args.presets:
         args.presets = list(PRESETS.values())
-    generate_database(args.database_file, args.rows, args.presets)
+    generate_database(args.database_file, args.rows, args.presets, args.count, args.language, args.name)
 
 if __name__ == "__main__":
     main()
